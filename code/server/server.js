af57fe737997dd248841a830be5dcc17a7277662
ALESSIO SALTA s296191 <s296191@studenti.polito.it>
2022-05-11 19:08:29 +0200

    added hardcoded users and fixed typo in server.js

--- code/server/server.js
+++ code/server/server.js
@@ -33,7 +33,7 @@ const U = new User(db);
 const TD = new Test_Descriptor(db);
 const TR = new Test_Result(db);
 
-dbHelper.create_sku_table().then(function(response){console.log(response);}, function(error){console.error( error);}); // to check
+db.create_sku_table().then(function(response){console.log(response);}, function(error){console.error( error);}); // to check
 
 // init express
 const app = new express();
