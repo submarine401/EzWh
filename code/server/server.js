12f32e642b3493a5353e7bf0a42917ab1b13b54a
unknown <s301584@studenti.polito.it>
2022-05-13 09:54:48 +0200

    server updated

--- code/server/server.js
+++ code/server/server.js
@@ -2,16 +2,24 @@
 const express = require('express');
 const db = require('./DBhelper');
 const InternalOrder = require('./InternalOrder');
-const Item = require('./Item')
+const Item = require('./Item');
 const ReturnOrder = require('./ReturnOrder');
+const Restockorder = require('./Restockorder');
 const SKU = require('./SKU');
-const DataInterface = require('./DataInterface');
-const User = require('./User');
+const dataInterface = require('./DataInterface');
+const UserAPI = require('./api/UserAPI');
 const SKUapi = require('./api/SKUapi');
 const PositionApi = require('./api/PositionApi');
+const ItemAPI = require('./api/ItemAPI')
+const RestockOrederAPI = require('./api/RestockOrederAPI')
+const ReturnOrederAPI = require('./api/ReturnOrderAPI');
+const InternalOrderAPI = require('./api/InternalOrderAPI');
+
 const Test_Descriptor = require('./Test_Descriptor');
 const Test_Result= require('./Test_Result');
-const dbHelper = require('./DBhelper');
+const SKUItemAPI = require('./api/SKUItemAPI');
+
+
 
 
 /*
@@ -22,14 +30,13 @@ Connect to DB
 Creating instances of classe which db connection is passed to each one
 */
 //const dataInterface = new DataInterface(db);
-const IO = new InternalOrder(db);
-const I = new Item(db);
-const RO = new ReturnOrder(db);
-const U = new User(db);
-const TD = new Test_Descriptor(db);
-const TR = new Test_Result(db);
-
-dbHelper.create_sku_table().then(function(response){console.log(response);}, function(error){console.error( error);}); // to check
+// const IO = new InternalOrder(db);
+// const I = new Item(db);
+// const RO = new ReturnOrder(db);
+// const RSO = new Restockorder(db);
+// const U = new User(db);
+// const TD = new Test_Descriptor(db);
+// const TR = new Test_Result(db);
 
 // init express
 const app = new express();
@@ -38,346 +45,15 @@ app.use(express.json());
 /*
 ***************************************** IO API ****************************************************
 */
-
-/*
-GET IO
-*/
-app.get('/api/internalOrders/:id',async (req,res)=>{ 
-  try
-    {     
-          const id = req.params.id 
-          if(id > 0)
-          {
-            const results = await IO.get_internalOrders(id);
-
-            if (results.length ===0)
-              return res.status(404).json("no internal order associated to id)"); 
-            else
-              return res.status(200).json(results); 
-          } 
-    }
-  catch(err)
-  {
-    console.log(err);
-    return res.status(500).end();
-  }
-});
-
-app.get('/api/internalOrders',async (req,res)=>{
-  try
-    {     
-         
-            const results = await IO.get_internalOrders();
-            return res.status(200).json(results);
-          
-    }
-  catch(err)
-  {
-    console.log(err);
-    return res.status(500).end();
-  }
-});
-
-
-app.get('/api/internalOrdersIssued',async (req,res)=>{
-  try
-    {
-      const results = await IO.get_issued_internalOrders();
-      return res.status(200).json(results);
-    }
-  catch(err)
-  {
-    console.log(err);
-    return res.status(500).end();
-  }
-}); 
-
-app.get('/api/internalOrdersAccepted',async (req,res)=>{
-  try
-    {
-      const results = await IO.get_acceped_internalOrders();
-      return res.status(200).json(results);   
-    }
-  catch(err)
-  {
-    console.log(err);
-    return res.status(500).end();
-  }
-});
-
-/*
-POST IO
-*/
-app.post('/api/internalorders',async (req,res)=>{
-  try{
-
-    if(Object.keys(req.body).length === 0){
-      return res.status(422).json({error : "Unprocessable Entity"});
-    }
-  
-    const nio = req.body.nio;
-    if( nio === undefined  || nio.customerId === undefined || nio.products === undefined ){
-      return res.status(422).json({error : "Unprocessable Entity"});
-    }
-  
-  const results = await IO.insert_internal_order(nio);
-  return res.status(200).json(results);
-  }
-  catch(err)
-  {
-    console.log(err);
-    return res.status(503).end();
-  }
-});
-/*
-DELETE IO
-*/
-app.delete('/api/internalOrders/:id',async (req,res)=>{
-  try{
-
-   
-  
-    const id = req.params.id
-    if( id === undefined ){
-      return res.status(422).json({error : "INVALID IO INPUT"});
-    }
-  
-  const results = await IO.delete_internal_order(id);
-  return res.status(200).json(results);
-  }
-  catch(err)
-  {
-    console.log(err);
-    return res.status(503).end();
-  }
-});
-/*
-MODIFY IO
-*/
-app.put('/api/internalOrders/:id',async (req,res)=>{
-  try{
-
-   
-  
-    const io = req.body.io;
-    if(Object.keys(req.body).length === 0 || io === undefined || io.newState === undefined ){
-      return res.status(422).json({error : "Unprocessable Entity"});
-    }
-    
-    const id = req.params.id
-    const results = await IO.
-    ernal_order(id,io);
-    return res.status(200).json(results);
-
-  }
-  catch(err)
-  {
-    console.log(err);
-    return res.status(500).end();
-  }
-});
-
-/*
-***************************************** ITEM API ****************************************************
-*/
-
-/*
-INSERT NEW ITEM
-*/
-app.post('/api/item',async (req,res)=>{
-  try{
-
-    if(Object.keys(req.body).length === 0){
-      return res.status(422).json({error : "Unprocessable Entity"});
-    }
-  
-    const ni = req.body.ni;
-    if( ni === undefined || ni.description === undefined || ni.price === undefined || ni.SKUId === undefined || ni.supplierId === undefined ){
-      return res.status(422).json({error : "Unprocessable Entityy"});
-    }
-  
-    const results = await I.insert_into_item_table(ni);
-    return res.status(200).json(results);
-  // @@@@@@@@@@@@@@@@@@@ 404 error needs to be handeled because of SKUid
-  }
-  catch(err)
-  {
-    console.log(err);
-    return res.status(503).end();
-  }
-});
-
-/*
-MODIFY I
-*/
-app.put('/api/item/:id',async (req,res)=>{
-  try
-    {
-      const i = req.body.i;
-      if(Object.keys(req.body).length === 0 || i === undefined || i.newDescription === undefined || i.newPrice === undefined ){
-        return res.status(422).json({error : "Unprocessable Entity"});
-      }
-      
-      const id = req.params.id
-      if(id >0)
-      {
-        const results = await I.modify_item(id,i);
-        return res.status(200).json(results);
-      }
-      else 
-      {
-        return res.status(422).json({error : "especify id of item"});
-      }
-    }
-  catch(err)
-  {
-    console.log(err);
-    return res.status(500).end();
-  }
-});
-/*
-DELETE I
-*/
-app.delete('/api/item/:id',async (req,res)=>{
-  try{
-    const id = req.params.id
-    if( id === undefined ){
-      return res.status(422).json({error : "INVALID IO INPUT"});
-    }
-  
-  const results = await I.delete_item(id);
-  return res.status(200).json(results);
-  }
-  catch(err)
-  {
-    console.log(err);
-    return res.status(503).end();
-  }
-});
-/*
-GET ALL I
-*/
-app.get('/api/item',async (req,res)=>{
-
-  //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 404 Not Found (Sku not found)
-  try
-    {     
-      const results = await I.get_all_items();
-      return res.status(200).json(results);
-    }
-  catch(err)
-  {
-    console.log(err);
-    return res.status(500).end();
-  }
-});
-
-app.get('/api/item/:id',async (req,res)=>{
-  try
-    {     
-
-      const id = req.params.id
-      if( id > 0 )
-      {
-      const results = await I.get_item_by_id(id);
-      return res.status(200).json(results);
-      }
-      else
-      {
-        return res.status(422).json({error : "INVALID I INPUT"});
-      }
-    }
-  catch(err)
-  {
-    console.log(err);
-    return res.status(500).end();
-  }
-});
+app.use('/',InternalOrderAPI);
 /*
 ***************************************************** RO API *******************************************************
 */
+app.use('/',ReturnOrederAPI);
 /*
-INSERT NEW RO
+*****************************************RSO API *****************************************************
 */
-app.post('/api/returnOrder',async (req,res)=>{
-  try{
-
-    if(Object.keys(req.body).length === 0){
-      return res.status(422).json({error : "Unprocessable Entity"});
-    }
-
-    const nro = req.body.nro;
-    if( nro === undefined || nro.returnDate === undefined || nro.products === undefined || nro.restockOrderId === undefined ){
-      return res.status(422).json({error : "Unprocessable Entityy"});
-    }
-  
-    const results = await RO.insert_return_order_table(nro);
-    return res.status(200).json(results);
-  
-  }
-  catch(err)
-  {
-    console.log(err);
-    return res.status(503).end();
-  }
-});
-/*
-DELETE RO
-*/
-app.delete('/api/returnOrder/:id',async (req,res)=>{
-  try{
-    const id = req.params.id
-    if( id === undefined ){
-      return res.status(422).json({error : "INVALID IO INPUT"});
-    }
-  
-  const results = await RO.delete_item(id);
-  return res.status(200).json(results);
-  }
-  catch(err)
-  {
-    console.log(err);
-    return res.status(503).end();
-  }
-});
-/*
-GET ALL RO
-*/
-app.get('/api/returnOrders',async (req,res)=>{
-
-  try
-    {     
-      const results = await RO.get_all_RO();
-      return res.status(200).json(results);
-    }
-  catch(err)
-  {
-    console.log(err);
-    return res.status(500).end();
-  }
-});
-
-app.get('/api/returnOrders/:id',async (req,res)=>{
-  try
-    {     
-
-      const id = req.params.id
-      if( id > 0 )
-      {
-      const results = await RO.get_all_RO_by_id(id);
-      return res.status(200).json(results);
-      }
-      else
-      {
-        return res.status(422).json({error : "INVALID I INPUT"});
-      }
-    }
-  catch(err)
-  {
-    console.log(err);
-    return res.status(500).end();
-  }
-});
+app.use('/',RestockOrederAPI);
 
 /*
 ***************************************** SKU API ****************************************************
@@ -391,133 +67,26 @@ app.use('/', SKUapi);
 
 app.use('/', PositionApi);
 
-
 /****************************USER API******************************/
 
-app.get('/api/suppliers', async (req,res) =>{
-  try {
-    
-    const results= await U.getSuppliers();
-    return res.status(200).json(results);
-    
-  } catch (err) {
-    //MISSING ERROR 401 (NOT AUTHENTICATED)
-    console.log(err);
-    return res.status(500).end();
-  }
-});
+app.use('/',ItemAPI)
+app.use('/',UserAPI);
 
-app.get('/api/users', async (req,res) => {
-  try{
-    const result = await DataInterface.getUsers_except_manager();
-    return res.status(200).json(result);
-  }
-  catch(err){
-    //MISSING ERROR 401 (NOT AUTHENTICATED)
-    console.log(err);
-    return res.status(500).end();
-  }
-});
-
-app.post('/api/newUser', async (req,res) => {
-  try {
-    
-    const new_u = req.body;
-    let users_array = [];
-    
-    if( new_u.username === undefined ||  new_u.password === undefined ||  new_u.name === undefined || new_u.surname === undefined || new_u.type === undefined){
-      return res.status(522).end("Unprocessable entity");
-    }
-    
-    const check_username = await DataInterface.getUsers();
-    const res_check_username = check_username.filter(function(users){
-      return users.username == new_u.username;
-    });
-    
-    if(res_check_username.length !== 0){
-      return res.status(409).end("User already existent!");
-    }
-    const result = await U.newUser(new_u);
-    return res.status(200).end("User inserted!");
-    
-
-  } catch (err) {
-    console.log(err);
-    return res.status(503).end();
-  }
-});
-
-
-app.put('/api/users/:username', async (req,res) =>{
-  
-  try {
-    const body = req.body;
-    const username = req.params.username;
-    if (body.oldType === body.newType ||
-        body.oldType === 'manager'){
-      return res.status(422).end("attempt to modify manager!");
-    }
-    if(typeof req.params.username === undefined 
-      || typeof req.body.oldType !== 'string' 
-      || typeof req.body.newType !== 'string'){
-      return res.status(422).end();
-    }
-    
-    const check_type = await DataInterface.getUsers();
-    const res_check_type = check_type.filter(function(users){
-      return (users.type == body.oldType && users.username == username);
-    });
-    
-    if(res_check_type.length === 0){
-      return res.status(404).end("Wrong username or OldType");
-    }
-    
-    const result = await U.modify_user_rights(username,body.newType);
-    return res.status(200).end();
-    
-    
-  } catch (err) {
-    console.log(err);
-    return res.status(503).end();
-  }  
-});
-
-
-app.delete('/api/users/:username/:type', async (req,res) => {
-  const type = req.params.type;
-  const username = req.params.username
-  try {
-    if(username === undefined || type === undefined || type === "administrator" || type === "administrator"){
-      console.log("Unprocessable entity\n");
-      return res.status(422).end();
-    }
-    
-    const result = await U.deleteUser(username,type);
-    return res.status(204).end();
-    
-  } catch (err) {
-    console.log(err);
-    return res.status(503).end();
-  }
-  
-});
-
+/*****************************SKUITEM API***************************/
+app.use('/', SKUItemAPI);
 
 
 /*
 ***************************************** Test Descriptor API ****************************************************
 */
 
-
-app.use('/', TestDescrptorAPI);
+app.use('/', TestDescriptorAPI);
 
 /*
 ***************************************** Test Result API ****************************************************
 */
 
-
 app.use('/', TestResultAPI);
-    
 
 // activate the server
 app.listen(port, () => {
