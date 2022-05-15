d88311190eb2700fcd8c08586b6e655e3735aa41
GABRIELE DI BARTOLOMEI s300273 <s300273@student.polito.it>
2022-05-15 18:36:31 +0200

    added some validation on sku api

--- code/server/DataInterface.js
+++ code/server/DataInterface.js
@@ -72,6 +72,13 @@ class DataInterface{
 
     delete_SKU(id){
         console.log('delete SKU ' + id);
+
+        const skuItems = await this.get_all_SKUItem();
+        if(skuItems.find(si => si.SKUid === id)){
+            console.log('sku has skuItems');
+            throw 'cannot delete'
+        }
+
         this.get_SKU(id).then( sku => {
             if(sku !== undefined){
                 this.dbHelper.delete_SKU(id);
