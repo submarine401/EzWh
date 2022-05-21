0d6bad622ac271a08520b2ed7151b32e30d0c001
unknown <s301584@studenti.polito.it>
2022-05-21 18:07:19 +0200

    added mock tr and td

---
+++ code/server/modules/mock_tdDAO.js
@@ -0,0 +1,5 @@
+exports.insert_into_test_Descriptor_table=jest.fn();
+exports.modify_test_descriptor=jest.fn();
+exports.delete_test_descriptor=jest.fn();
+exports.get_TD=jest.fn();
+exports.get_TD_by_id=jest.fn();
