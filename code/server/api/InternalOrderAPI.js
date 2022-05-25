4b6e746bc99cfb3078a5fce139d40e9213463b38
Masoud <s300283@studenti.polito.it>
2022-05-25 19:14:04 +0200

    minor fixes

--- code/server/api/InternalOrderAPI.js
+++ code/server/api/InternalOrderAPI.js
@@ -23,8 +23,12 @@ router.get('/api/internalOrders/:id',async (req,res)=>{
   
               if (results.length ===0)
                 return res.status(404).json("no internal order associated to id"); 
-              else
+              else{
+              if(results===0)
+              return res.status(404).json("no internal order associated to id"); 
+                else
                 return res.status(200).json(results); 
+              }
             } 
       }
     catch(err)
