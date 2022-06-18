c2e8af4eac02b546511333a278b010eb7155b767
Masoud <s300283@studenti.polito.it>
2022-06-18 23:40:01 +0200

    change item API

--- code/server/api/ItemAPI.js
+++ code/server/api/ItemAPI.js
@@ -58,19 +58,52 @@ router.get('/api/items',async (req,res)=>{
     }
   });
 
-  router.put('/api/item/:id',async (req,res)=>{
+  // router.put('/api/item/:id',async (req,res)=>{
+  //   try
+  //     {
+  //       const i = req.body;
+  //       if(Object.keys(req.body).length === 0 || i === undefined || i.newDescription === undefined || i.newPrice === undefined ){
+  //         return res.status(422).json({error : "Unprocessable Entity"});
+  //       }
+        
+  //       const id = req.params.id
+  //       if(id >= 0)
+  //       {  
+  //         const results = await itemservice.getItembyId(id).then((suc)=> {if(suc!==0) return itemservice.modifyItem(id,i); else return suc }, (error)=> {return 0;});
+  //         if(results !== 0 )
+  //           return res.status(200).json(results);
+  //         else
+  //           return res.status(404).json({error : "Item not existing)"});
+          
+  //       }
+  //       else 
+  //       {
+  //         return res.status(422).json({error : "especify id of item"});
+  //       }
+  //     }
+  //   catch(err)
+  //   {
+  //     console.log(err);
+  //     return res.status(500).end();
+  //   }
+  // });
+
+  router.put('/api/item/:id/:supplierId',async (req,res)=>{
     try
       {
         const i = req.body;
+        
+
         if(Object.keys(req.body).length === 0 || i === undefined || i.newDescription === undefined || i.newPrice === undefined ){
           return res.status(422).json({error : "Unprocessable Entity"});
         }
         
         const id = req.params.id
+        const suplierid = req.params.supplierId
         if(id >= 0)
-        {
-  
-          const results = await itemservice.getItembyId(id).then((suc)=> {if(suc!==0) return itemservice.modifyItem(id,i); else return suc }, (error)=> {return 0;});
+        {  
+          // const results = await itemservice.getItembyId(id).then((suc)=> {if(suc!==0) return itemservice.modifyItem(id,i); else return suc }, (error)=> {return 0;});
+          const results = await itemservice.getItembyIdAndSupplierId(id, suplierid).then((suc)=> {if(suc!==0) return itemservice.modifyItem(id,i); else return suc }, (error)=> {return 0;});
           if(results !== 0 )
             return res.status(200).json(results);
           else
@@ -90,12 +123,34 @@ router.get('/api/items',async (req,res)=>{
   });
 
 
-  router.delete('/api/items/:id',async (req,res)=>{
+
+  // router.delete('/api/items/:id',async (req,res)=>{
+  //   try{
+  //     const id = req.params.id
+     
+  //     if( id >= 0 ){
+  //       const results = await itemservice.deleteItem(id);
+        
+  //       return res.status(204).json(results);
+       
+  //     }
+  //     return res.status(422).json({error : "INVALID IO INPUT"});
+    
+  //   }
+  //   catch(err)
+  //   {
+  //     console.log(err);
+  //     return res.status(503).end();
+  //   }
+  // });
+  
+  router.delete('/api/items/:id/:supplierId',async (req,res)=>{
     try{
       const id = req.params.id
+      const suplierid = req.params.supplierId
      
       if( id >= 0 ){
-        const results = await itemservice.deleteItem(id);
+        const results = await itemservice.deleteItem(id,suplierid);
         
         return res.status(204).json(results);
        
@@ -135,5 +190,34 @@ router.get('/api/items',async (req,res)=>{
       return res.status(500).end();
     }
   });
+
+
+  router.get('/api/items/:id/:supplierId',async (req,res)=>{
+    try
+      {     
+  
+        const id = req.params.id
+        const supplierId = req.params.supplierId
+        if( id >= 0 && supplierId>=0 )
+        {
+        
+          const results = await itemservice.getItembyIdAndSupplierId(id, supplierId);
+          if(results != -1)
+            return res.status(200).json(results);
+          else
+            return res.status(404).json("no item associated to id");
+        }
+        else
+        {
+          return res.status(422).json({error : "INVALID I INPUT"});
+        }
+      }
+    catch(err)
+    {
+      console.log(err);
+      return res.status(500).end();
+    }
+  });
+
 
 module.exports = router;
